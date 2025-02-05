2832055d251326d90caa3122921b63048fe81fbe
Nguyen Khang <nguyengiakhang646@gmail.com>
2025-02-05 16:22:49 +0700

    refactor(students): Remove manual ID assignment for POST request

--- server/routes/api/students.js
+++ server/routes/api/students.js
@@ -4,25 +4,24 @@ const Student = require("../../models/student");
 
 router.get("/", async (req, res) => {
   const students = await Student.find();
-  res.send(students);
+  res.json({ students });
 });
 
 router.get("/:id", async (req, res) => {
   const student = await Student.findById(req.params.id);
-  res.send(student);
+  res.json({ student });
 });
 
 router.post("/", async (req, res) => {
-  const { id, name, studentNumber, program, timestamp } = req.body;
+  const { name, studentNumber, program, timestamp } = req.body;
   const student = new Student({
-    _id: id,
     name,
     studentNumber,
     program,
     createdAt: new Date(timestamp),
   });
   await student.save();
-  res.send(student);
+  res.json({ student });
 });
 
 router.put("/:id", async (req, res) => {
@@ -37,12 +36,12 @@ router.put("/:id", async (req, res) => {
     },
     { new: true }
   );
-  res.send(student);
+  res.json({ student });
 });
 
 router.delete("/:id", async (req, res) => {
   await Student.findByIdAndDelete(req.params.id);
-  res.send({ message: "Student deleted" });
+  res.json({ message: "Student deleted" });
 });
 
 module.exports = router;
